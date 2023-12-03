79f6f3276b6c62385d356e3578869392cb5ed7b0
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-03 14:48:08 +1100

    Day 2 part 2

---
+++ 2023/02/part2.ts
@@ -0,0 +1,24 @@
+export default function (input: string) {
+	return input
+		.trim()
+		.split('\n')
+		.map((line) => {
+			const mins: { [key: string]: number } = {
+				red: 0,
+				green: 0,
+				blue: 0
+			};
+			const handfulls = line
+				.substring(line.indexOf(':') + 2)
+				.split('; ')
+				.map((handfull) => handfull.split(', '));
+			for (const handfull of handfulls) {
+				for (const cubes of handfull) {
+					const [count, colour] = cubes.split(' ');
+					mins[colour] = Math.max(mins[colour], parseInt(count));
+				}
+			}
+			return mins.red * mins.green * mins.blue;
+		})
+		.reduce((sum, value) => sum + value);
+}
