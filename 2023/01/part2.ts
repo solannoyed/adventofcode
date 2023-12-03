0a4709429cc52c9dadde8771a2d07f0d20244e22
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-03 20:54:16 +1100

    Refactor to use library exports

--- 2023/01/part2.ts
+++ 2023/01/part2.ts
@@ -1,27 +1,4 @@
-// It would be nice to just use the exact same function as part 1, but `const NUMS` is different and it wont use the one in this file.
-// import process from './part1';
-// export default process;
-
-const NUMS = [
-	{ string: '1', value: 1 },
-	{ string: 'one', value: 1 },
-	{ string: '2', value: 2 },
-	{ string: 'two', value: 2 },
-	{ string: '3', value: 3 },
-	{ string: 'three', value: 3 },
-	{ string: '4', value: 4 },
-	{ string: 'four', value: 4 },
-	{ string: '5', value: 5 },
-	{ string: 'five', value: 5 },
-	{ string: '6', value: 6 },
-	{ string: 'six', value: 6 },
-	{ string: '7', value: 7 },
-	{ string: 'seven', value: 7 },
-	{ string: '8', value: 8 },
-	{ string: 'eight', value: 8 },
-	{ string: '9', value: 9 },
-	{ string: 'nine', value: 9 }
-];
+import { NUMBERS } from 'lib/numbers';
 
 export default function (input: string) {
 	return input
@@ -36,17 +13,17 @@ export default function (input: string) {
 				index: -1,
 				value: 0
 			};
-			for (const num of NUMS) {
-				let index = line.indexOf(num.string);
+			for (const number of NUMBERS) {
+				let index = line.indexOf(number.string);
 				if (index < 0) continue;
 				if (index < first.index) {
 					first.index = index;
-					first.value = num.value;
+					first.value = number.value;
 				}
-				index = line.lastIndexOf(num.string);
+				index = line.lastIndexOf(number.string);
 				if (index > last.index) {
 					last.index = index;
-					last.value = num.value;
+					last.value = number.value;
 				}
 			}
 			return first.value * 10 + last.value;
