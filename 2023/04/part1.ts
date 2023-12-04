f85836108761768ab53604031e30787d6ea7edf5
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-04 20:50:21 +1100

    More functional, because why not 🤷‍♂️

--- 2023/04/part1.ts
+++ 2023/04/part1.ts
@@ -1,21 +1,18 @@
 export default function (input: string) {
-	let result = 0;
-	for (const line of input.trim().split('\n')) {
-		const [winning, selected] = line
-			.substring(line.indexOf(':') + 1)
-			.split('|')
-			.map((numbers) =>
-				numbers
-					.trim()
-					.split(' ')
-					.filter((number) => number.length > 0)
-					.map((number) => parseInt(number))
-			);
-		let count = selected.filter((number) => winning.includes(number)).length;
-		if (count > 0) {
-			result += 2 ** (count - 1);
-		}
-	}
-	return result;
+	return input
+		.trim()
+		.split('\n')
+		.map((line) =>
+			line
+				.substring(line.indexOf(':') + 1)
+				.split('|')
+				.map((numbers) =>
+					numbers
+						.split(' ')
+						.filter((num) => num.length > 0)
+						.map((num) => parseInt(num))
+				)
+		)
+		.map(([winning, selected]) => selected.filter((number) => winning.includes(number)).length)
+		.reduce((previous, current) => previous + ((1 << current) >> 1), 0);
 }
-
--- 2023/04/part2.ts
+++ 2023/04/part2.ts
@@ -1,21 +1,26 @@
 export default function (input: string) {
-	const lines = input.trim().split('\n');
-	const cards = new Array<number>(lines.length).fill(1);
-	for (let line = 0; line < lines.length; line++) {
-		const [winning, selected] = lines[line]
-			.substring(lines[line].indexOf(':') + 1)
-			.split('|')
-			.map((numbers) =>
-				numbers
-					.trim()
-					.split(' ')
-					.filter((number) => number.length > 0)
-					.map((number) => parseInt(number))
-			);
-		const count = selected.filter((number) => winning.includes(number)).length;
-		for (let card = line + 1; card <= line + count; card++) {
-			cards[card] += cards[line];
-		}
-	}
-	return cards.reduce((accumulator, value) => accumulator + value);
+	return input
+		.trim()
+		.split('\n')
+		.map((line) =>
+			line
+				.substring(line.indexOf(':') + 1)
+				.split('|')
+				.map((numbers) =>
+					numbers
+						.split(' ')
+						.filter((number) => number.length > 0)
+						.map((number) => parseInt(number))
+				)
+		)
+		.map(([winning, selected]) => {
+			return { count: 1, wins: selected.filter((number) => winning.includes(number)).length };
+		})
+		.map(({ count, wins }, index, cards) => {
+			for (let card = index + wins; card > index; card--) {
+				cards[card].count += count;
+			}
+			return count;
+		})
+		.reduce((accumulator, value) => accumulator + value);
 }
