0fbc19bdcd8337ca5d54e80cba7408aa3a6ff289
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-09 23:13:32 +1100

    Day 9 part 1

---
+++ 2023/09/part1.ts
@@ -0,0 +1,24 @@
+export default function (input: string) {
+	const histories = input
+		.trim()
+		.split('\n')
+		.map((line) => [line.split(' ').map((num) => parseInt(num))]);
+	for (const history of histories) {
+		for (let step = 0; step < history.length; step++) {
+			if (!history[step].some((value) => value != 0)) break;
+			let next: number[] = [];
+			for (let index = 1; index < history[step].length; index++) {
+				next.push(history[step][index] - history[step][index - 1]);
+			}
+			history.push(next);
+		}
+
+		for (let index = history.length - 1; index > 0; index--) {
+			history[index - 1].push(history[index - 1].at(-1)! + history[index].at(-1)!);
+		}
+	}
+
+	return histories.reduce((accumulator, history) => {
+		return accumulator + history[0].at(-1)!;
+	}, 0);
+}
