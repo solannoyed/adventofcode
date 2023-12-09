7b0bc0edd9aa011bb8658db3e453dfd435006119
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-09 23:55:40 +1100

    Refactor Day 7

--- 2023/07/part1.ts
+++ 2023/07/part1.ts
@@ -1,28 +1,13 @@
-export default function (input: string) {
+export default function (input: string, deck = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'], joker = -1) {
 	const hands = input
 		.trim()
 		.split('\n')
 		.map((line) => line.trim().split(' '))
 		.map(([cards, bid]) => {
-			const hand = cards.split('').map((card) => {
-				switch (card) {
-					case 'A':
-						return 14;
-					case 'K':
-						return 13;
-					case 'Q':
-						return 12;
-					case 'J':
-						return 11;
-					case 'T':
-						return 10;
-					default:
-						return parseInt(card);
-				}
-			});
+			const hand = cards.split('').map((card) => deck.indexOf(card));
 			return {
 				hand,
-				type: getType(hand),
+				type: getType(hand, joker),
 				bid: parseInt(bid)
 			};
 		});
@@ -38,15 +23,19 @@ export default function (input: string) {
 	}, 0);
 }
 
-function getType(hand: number[]) {
+function getType(hand: number[], joker: number) {
 	let map = new Map<number, number>();
 	for (const card of hand) {
 		map.set(card, (map.get(card) ?? 0) + 1);
 	}
+	const jokers = map.get(joker) ?? 0;
+	map.delete(joker);
+
 	const counts = [...map.values()];
 	counts.sort((first, second) => second - first);
+	counts[0] += jokers;
 
-	if (counts[0] == 5) return 0; // five of a kind
+	if (jokers == 5 || counts[0] == 5) return 0; // five of a kind
 	if (counts[0] == 4) return 1; // four of a kind
 	if (counts[0] == 3 && counts[1] == 2) return 2; // full house
 	if (counts[0] == 3) return 3; // three of a kind
