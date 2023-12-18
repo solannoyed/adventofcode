9cc841df9d4d3559bad8d71c9e3ab81a535e9128
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-18 15:47:16 +1100

    Day 17 part 2

---
+++ 2023/17/part2.ts
@@ -0,0 +1,56 @@
+import { CARDINALS } from 'lib/directions';
+
+export default function (input: string) {
+	const blocks = input
+		.trim()
+		.split('\n')
+		.map((line) => line.split('').map((block) => parseInt(block)));
+	const coolest: number[][][] = new Array(blocks.length); // x, y, direction
+	for (let row = 0; row < coolest.length; row++) {
+		coolest[row] = new Array(blocks[row].length);
+		for (let col = 0; col < coolest[row].length; col++) {
+			coolest[row][col] = new Array(4).fill(Infinity);
+		}
+	}
+
+	const queue: Crucible[] = [
+		{ x: 0, y: 0, direction: 1, heat: 0 },
+		{ x: 0, y: 0, direction: 2, heat: 0 }
+	];
+	while (queue.length > 0) {
+		const current = queue.shift()!; // make sure the BFS!
+		if (coolest[current.y][current.x][current.direction] <= current.heat) continue;
+		coolest[current.y][current.x][current.direction] = current.heat;
+
+		for (const distance of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+			current.x += CARDINALS[current.direction].x;
+			current.y += CARDINALS[current.direction].y;
+			if (!blocks[current.y] || !blocks[current.y][current.x]) break; // outside the grid
+			current.heat += blocks[current.y][current.x];
+
+			if (distance < 4) continue;
+			queue.push({
+				x: current.x,
+				y: current.y,
+				direction: (current.direction + 3) % 4, // left turn
+				heat: current.heat
+			});
+			queue.push({
+				x: current.x,
+				y: current.y,
+				direction: (current.direction + 1) % 4, // right turn
+				heat: current.heat
+			});
+		}
+	}
+
+	return Math.min(...coolest.at(-1)!.at(-1)!);
+}
+
+type Crucible = {
+	x: number; // column in the city blocks
+	y: number; // row in the city blocks
+	direction: number; // cardinal direction index
+	// travelled: number; // blocks travelled in the same direction
+	heat: number; // the amount of heat lost in the trip
+};
