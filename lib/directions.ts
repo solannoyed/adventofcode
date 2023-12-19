a649acc67b075073a8f78a3445cd71466875c55a
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-19 13:22:31 +1100

    Always use North/Up as the starting point for directions

--- lib/directions.ts
+++ lib/directions.ts
@@ -1,12 +1,12 @@
 export const DIRECTIONS = [
-	{ x: -1, y: -1 },
 	{ x: 0, y: -1 },
 	{ x: 1, y: -1 },
 	{ x: 1, y: 0 },
 	{ x: 1, y: 1 },
 	{ x: 0, y: 1 },
 	{ x: -1, y: 1 },
-	{ x: -1, y: 0 }
+	{ x: -1, y: 0 },
+	{ x: -1, y: -1 }
 ];
 
 export const CARDINALS = [
