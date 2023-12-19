74b3ab5a773122ba7d2630ca86b48e905c02615f
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-19 17:57:59 +1100

    Day 19 part 1

---
+++ 2023/19/part1.ts
@@ -0,0 +1,63 @@
+export default function (input: string) {
+	const groups = input
+		.trim()
+		.split('\n\n')
+		.map((group) => group.split('\n'));
+
+	const workflows: Map<string, Rule[]> = new Map();
+	groups[0].forEach((line) => {
+		const [name, ...rest] = line.substring(0, line.length - 1).split(/[{,]/);
+		workflows.set(name, getRules(rest));
+	});
+
+	const parts: Part[] = groups[1].map((line) =>
+		JSON.parse(
+			line.replaceAll('=', ':').replaceAll('{', '{"').replaceAll(':', '":').replaceAll(',', ',"')
+		)
+	);
+
+	let result = 0;
+	for (const part of parts) {
+		let destination = 'in';
+		rule: while (destination != 'A' && destination != 'R') {
+			for (const rule of workflows.get(destination)!) {
+				if (rule.operator == '<' && part[rule.category!] >= rule.value!) continue;
+				if (rule.operator == '>' && part[rule.category!] <= rule.value!) continue;
+				destination = rule.destination;
+				continue rule;
+			}
+		}
+		if (destination == 'A') {
+			result += part.x + part.m + part.a + part.s;
+		}
+	}
+
+	return result;
+}
+
+function getRules(input: string[]) {
+	return input.map((item) => {
+		const colon = item.indexOf(':');
+		if (colon < 0) return { destination: item } as Rule;
+		return {
+			category: item[0],
+			operator: item[1],
+			value: parseInt(item.substring(2, colon)),
+			destination: item.substring(colon + 1)
+		} as Rule;
+	});
+}
+
+type Part = {
+	[key in Category]: number;
+};
+
+type Rule = {
+	category?: Category;
+	operator?: Operator;
+	value?: number;
+	destination: string; // 'R': rejected | 'A': accepted | string: name
+};
+
+type Category = 'x' | 'm' | 'a' | 's';
+type Operator = '>' | '<';
