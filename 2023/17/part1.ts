c9428e794284275696354dcdfa19152b5ba816a7
sol <110832188+solannoyed@users.noreply.github.com>
2023-12-19 14:43:51 +1100

    Day 17: Check valid before adding to the queue (significant performance boost) and only check vertical/horizontal

--- 2023/17/part1.ts
+++ 2023/17/part1.ts
@@ -5,11 +5,11 @@ export default function (input: string) {
 		.trim()
 		.split('\n')
 		.map((line) => line.split('').map((block) => parseInt(block)));
-	const coolest: number[][][] = new Array(blocks.length); // x, y, direction
+	const coolest: number[][][] = new Array(blocks.length); // x, y, vertical/horizontal
 	for (let row = 0; row < coolest.length; row++) {
 		coolest[row] = new Array(blocks[row].length);
 		for (let col = 0; col < coolest[row].length; col++) {
-			coolest[row][col] = new Array(4).fill(Infinity);
+			coolest[row][col] = new Array(2).fill(Infinity);
 		}
 	}
 
@@ -18,15 +18,15 @@ export default function (input: string) {
 		{ x: 0, y: 0, direction: 2, heat: 0 }
 	];
 	while (queue.length > 0) {
-		const current = queue.shift()!; // make sure the BFS!
-		if (coolest[current.y][current.x][current.direction] <= current.heat) continue;
-		coolest[current.y][current.x][current.direction] = current.heat;
+		const current = queue.shift()!; // make sure to do BFS
 
 		for (const distance of [1, 2, 3]) {
 			current.x += CARDINALS[current.direction].x;
 			current.y += CARDINALS[current.direction].y;
 			if (!blocks[current.y] || !blocks[current.y][current.x]) break; // outside the grid
 			current.heat += blocks[current.y][current.x];
+
+			if (coolest[current.y][current.x][(current.direction + 1) % 2] <= current.heat) continue;
 
 			queue.push({
 				x: current.x,
@@ -40,6 +40,7 @@ export default function (input: string) {
 				direction: (current.direction + 1) % 4, // right turn
 				heat: current.heat
 			});
+			coolest[current.y][current.x][(current.direction + 1) % 2] = current.heat;
 		}
 	}
 
