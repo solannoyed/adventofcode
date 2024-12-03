bcc00d18c7deb321ce9a62fa326a5b34593c9f43
sol <110832188+solannoyed@users.noreply.github.com>
2024-12-04 01:01:20 +1100

    2024 Day 1 Part 2

---
+++ 2024/01/part2.ts
@@ -0,0 +1,17 @@
+export default function (input: string) {
+	const inputs = input
+		.trim()
+		.split('\n')
+		.map((line) => {
+			return line.split('   ').map((val) => parseInt(val));
+		});
+	const counts = new Map();
+	for (const [_, value] of inputs) {
+		counts.set(value, (counts.get(value) ?? 0) + 1);
+	}
+	let result = 0;
+	for (const [value, _] of inputs) {
+		result += value * (counts.get(value) ?? 0);
+	}
+	return result;
+}
