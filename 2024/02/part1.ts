b30a10655a974698785d315c9553d7b101fe2cb1
sol <110832188+solannoyed@users.noreply.github.com>
2024-12-04 01:02:32 +1100

    2024 Day 2 Part 1

---
+++ 2024/02/part1.ts
@@ -0,0 +1,26 @@
+export default function (input: string) {
+	let count = 0;
+	input
+		.trim()
+		.split('\n')
+		.map((line) => {
+			return line.split(' ').map((val) => parseInt(val));
+		})
+		.forEach((record) => {
+			let lower = 1,
+				upper = 3;
+			if (record[0] > record[1]) {
+				lower = -3;
+				upper = -1;
+			}
+			for (let index = 0; index < record.length - 1; index++) {
+				if (
+					record[index] + lower > record[index + 1] ||
+					record[index] + upper < record[index + 1]
+				)
+					return;
+			}
+			count++;
+		});
+	return count;
+}
