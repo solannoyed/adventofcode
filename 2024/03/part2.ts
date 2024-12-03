71ea8fdde1cfb4c527a44dc5f82462b37acde49a
sol <110832188+solannoyed@users.noreply.github.com>
2024-12-04 01:13:58 +1100

    Simplify 2024 Day 3 Part 2

--- 2024/03/part2.ts
+++ 2024/03/part2.ts
@@ -1,26 +1,18 @@
 export default function (input: string) {
 	let result = 0;
-
-	const regDoDont = /do(?:n't)?\(\)/g;
-	const dos = [0];
-	const donts = [-1];
-	for (const match of input.matchAll(regDoDont)) {
-		if (match[0] === 'do()') {
-			dos.push(match.index!);
-		} else donts.push(match.index!);
-	}
-
-	const regex = /mul\((\d+),(\d+)\)/g;
+	const regex = /(?<func>do|don't|mul)\((?:(?<first>\d+),(?<second>\d+))?\)/g;
+	let enabled = true;
 	for (const match of input.matchAll(regex)) {
-		const first = parseInt(match[1]);
-		const second = parseInt(match[2]);
-		const index = match.index!;
-		let doi = 0;
-		while (doi < dos.length - 1 && dos[doi + 1] < index) doi++;
-		let donti = 0;
-		while (donti < donts.length - 1 && donts[donti + 1] < index) donti++;
-		console.log(dos[doi], donts[donti], index);
-		if (dos[doi] > donts[donti]) result += first * second;
+		const func = match.groups?.['func'];
+		if (func === 'do') {
+			enabled = true;
+		} else if (func === "don't") {
+			enabled = false;
+		} else if (enabled) {
+			const first = parseInt(match.groups?.['first'] ?? '0');
+			const second = parseInt(match.groups?.['second'] ?? '0');
+			result += first * second;
+		}
 	}
 	return result;
 }
