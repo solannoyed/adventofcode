d9dd0b246312a2017407c35d2607f51338a2ce0e
sol <110832188+solannoyed@users.noreply.github.com>
2024-12-05 12:27:04 +1100

    Add ESLint packages and config

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...ts.configs.stylistic,
+	prettier,
+	{
+		rules: {
+			// for-of is slower and we (usually) need to focus on performance
+			'@typescript-eslint/prefer-for-of': 'off'
+		}
+	}
+);
